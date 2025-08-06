1ecedcb6bda21f66c2277328b7ac557ed3502bc7
Bereket Kume <135522204+bereket-kume@users.noreply.github.com>
2025-08-07 01:35:50 +0300

    Revert "completed repository methods for blog"

--- Infrastructure/database/mongo.go
+++ Infrastructure/database/mongo.go
@@ -2,44 +2,77 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type MongoDB struct {
+var (
 	Client *mongo.Client
-}
+	DB     *mongo.Database
+)
 
-func ConnectDB() (*MongoDB, error) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+// Connect establishes connection to MongoDB Atlas
+func Connect() error {
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI environment variable is not set")
 	}
-	mongoUrl := os.Getenv("URL")
-	if mongoUrl == "" {
-		log.Fatal("mongoUrl not set in .env file")
-	}
-	clientOptions := options.Client().ApplyURI(mongoUrl)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, clientOptions)
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	clientOptions.SetServerSelectionTimeout(30 * time.Second)
+	clientOptions.SetSocketTimeout(30 * time.Second)
+
+	var err error
+	Client, err = mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		return nil, fmt.Errorf("mongodb connection error: %v", err)
+		return err
 	}
 
-	fmt.Println("mongodb connected")
-	log.Println("mongodb connected")
-	return &MongoDB{Client: client}, nil
+	// Test the connection
+	err = Client.Ping(ctx, nil)
+	if err != nil {
+		log.Printf("Failed to ping MongoDB: %v", err)
+		log.Println("Please check:")
+		log.Println("1. Your IP address is whitelisted in MongoDB Atlas")
+		log.Println("2. Database user has correct permissions")
+		log.Println("3. Connection string format is correct")
+		return err
+	}
+
+	// Set the database
+	DB = Client.Database("blog_db")
+	log.Println("Successfully connected to MongoDB Atlas!")
+	return nil
 }
 
-func (db *MongoDB) GetCollection(databaseName, collectionName string) *mongo.Collection {
-	return db.Client.Database(databaseName).Collection(collectionName)
+// Disconnect closes the MongoDB connection
+func Disconnect() error {
+	if Client != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		return Client.Disconnect(ctx)
+	}
+	return nil
+}
+
+// GetCollection returns a MongoDB collection
+func GetCollection(collectionName string) *mongo.Collection {
+	return DB.Collection(collectionName)
+}
+
+// GetClient returns the MongoDB client
+func GetClient() *mongo.Client {
+	return Client
+}
+
+// GetDatabase returns the MongoDB database
+func GetDatabase() *mongo.Database {
+	return DB
 }
